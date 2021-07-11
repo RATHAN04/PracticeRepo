2758b76756fd333aecc16a8d8d2e1274f305b140
RATHAN04 <rathan0198@gmail.com>
2021-07-11 20:49:12 +0530

    New Branch Commit

---
+++ count.py
@@ -0,0 +1,8 @@
+count=0
+name="rathan"
+for i in name:
+    if i=="a":
+        count+=1
+
+print(count)
+    
---
+++ python.py
@@ -0,0 +1,7 @@
+name="banana"
+length=len(name)
+count=0
+while count<length:
+    letter=name[count]
+    print(letter)
+    count=count+1
