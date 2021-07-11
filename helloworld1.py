73f2aa2b010f091fadc1e50ae7383043e520c3f3
RATHAN04 <rathan0198@gmail.com>
2021-07-11 17:41:56 +0530

    Second commit

---
+++ helloworld1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 10:29:26 2021
+
+@author: Lenovo
+"""
+numbers = dict(x=5, y=0)
+print('numbers =', numbers)
+print(type(numbers))
+
+salinfo = dict(bangalore=56000, manglore=26000, mysore=78000)
+print(salinfo)
+if 'mangal' in salinfo:
+    print('yes')
+else:
+    print('np')
+
+for loc in salinfo:
+    print(salinfo[loc])
